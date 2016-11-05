bbb024231d938c2398c4f2cdf781a9d58c0f2ca3
saurabh <chavhansaurabh99@gmail.com>
2016-11-05 16:50:40 +0530

    ajax loading search results

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,14 @@ Rails.application.routes.draw do
   resources :images
   resources :users
   root 'subapps#index'
-  get '/search' => 'image_searches#search_and_upload_image', :as => 'image_search_app'
-  post '/search' => 'image_searches#search_and_upload_image', :as => 'search_and_upload_panel'
+
+  get '/search/:design_id' => 'image_searches#search_similar', as: 'search_similar'
+  get '/search/' => 'image_searches#search_and_upload_image', as: 'image_search_app'
+  post '/search/' => 'image_searches#search_and_upload_image', as: 'search_and_upload_panel'
   get 'api/get_similar_designs' => 'api#get_similar_designs'
-  post '/search/add_similar_design' => 'image_searches#add_similar_design', as: 'add_similar'
-  post '/search/remove_similar_design' => 'image_searches#remove_similar_design', as: 'remove_similar'
-  post '/search/check_similar' => 'image_searches#re_check_similar', as: 'recheck_similar'
+  post '/search/:design_id/add_similar_design' => 'image_searches#add_similar_design', as: 'add_similar'
+  post '/search/:design_id/remove_similar_design' => 'image_searches#remove_similar_design', as: 'remove_similar'
+  post '/search/:design_id/check_similar' => 'image_searches#re_check_similar', as: 'recheck_similar'
   get '/manage-catalogs/all' => 'manage_catalogs#remove_designs_from_catalog', as:"all_catalog"
   post '/manage-catalogs/all' => 'manage_catalogs#remove_designs_from_catalog', as:"remove_catalog"
   get 'api/international_catalog_designs' => 'api#international_catalog_designs'
